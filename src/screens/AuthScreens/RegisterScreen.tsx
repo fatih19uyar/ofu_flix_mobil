50e9dc82bed6ac8bb269a522a47b6d81c7f1625a
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-01-25 02:00:45 +0100

    AuthScreens added (empty)

---
+++ src/screens/AuthScreens/RegisterScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const RegisterScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>RegisterScreen</Text>
+    </View>
+  )
+}
+
+export default RegisterScreen
