8c4aaab10769900152cbb7e6142309452c7b9162
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-01-25 02:02:45 +0100

    type added.

---
+++ src/types/type.ts
@@ -0,0 +1,29 @@
+export type RootStackParamList = {
+  DashBoard: {} | undefined;
+  Login: {} | undefined;
+  Home: {} | undefined;
+  Register: {} | undefined;
+  TvShows: {} | undefined;
+  Movies: {} | undefined;
+  MyList: {} | undefined;
+  ModalCastConnect: {} | undefined;
+  TabNavigation: {} | undefined;
+  ModalAddProfile: {} | undefined;
+  MoreNotifications: {} | undefined;
+  MoreMyList: {} | undefined;
+  MoreAppSettings: {} | undefined;
+  ModalWebView: {url : string} | undefined;
+  ModalManageProfiles: {} | undefined;
+};
+
+export interface Show {
+  id: number;
+  title: string;
+  image: string;
+}
+
+export interface Data {
+  dumbData: Show[];
+  myList: (Show | { id: number; title: string; image?: undefined })[];
+  previews: Show[];
+}
