ec9a24a15c960493d697b93a3a6bdb55b2eee30e
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-01-29 01:30:21 +0100

    loginSlice updated.

--- src/store/login/loginSlice.ts
+++ src/store/login/loginSlice.ts
@@ -1,12 +1,9 @@
-// Import edilenleri ve diğer gerekli dosyaları ekleyin
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { login, logOut } from '../../common/services/auth';
-import { jwtDecode } from 'jwt-decode';
-import { LoginRequest, User } from '../../common/services/auth/type';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {LoginRequest, User} from '../../common/services/auth/type';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import useLocalStorage from '../../common/hooks/useLocalStorage';
+import {setLocalStorage} from '../../common/hooks/useLocalStorage';
+import data from '../../mockData/data';
 
-// Auth state tipini tanımlayın
 type AuthState = {
   user: User | null;
   token: string;
@@ -14,37 +11,43 @@ type AuthState = {
   userKey?: string;
 };
 
-// Auth state'inin başlangıç durumunu tanımlayın
 const initialState: AuthState = {
   user: null,
   token: '',
   remember: false,
 };
 
-// Redux thunk'larını ve slice'ı oluşturun
 export const loginAction = createAsyncThunk(
   'auth/login',
-  async (values: LoginRequest, { dispatch }): Promise<AuthState> => {
-    const data = await login(values);
-    const decodedUser: User = jwtDecode(data?.accessToken ?? '');
-
-    dispatch(setUser(decodedUser));
-    AsyncStorage.setItem(process.env.REACT_APP_ACCESS_TOKEN_KEY!, data?.accessToken ?? '');
-    AsyncStorage.setItem('user', JSON.stringify(decodedUser));
-    return {
-      token: data?.accessToken ?? '',
-      user: decodedUser,
-      remember: values.remember,
-      userKey: values.username,
-    };
+  async (values: LoginRequest, {dispatch}): Promise<AuthState> => {
+    try {
+      const user = data.userData.find(
+        user =>
+          user.username === values.username &&
+          user.password === values.password,
+      );
+      if (user === undefined) {
+        throw new Error('Invalid username or password');
+      } else {
+        dispatch(setUser(user));
+        setLocalStorage("token", user.token);
+        setLocalStorage('user', JSON.stringify(user));
+        return {
+          token: user.token,
+          user,
+          remember: values.remember,
+          userKey: values.username,
+        };
+      }
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
 );
 
 export const logoutAction = createAsyncThunk(
   'auth/logout',
-  async (_, { dispatch }) => {
-    await logOut();
-
+  async (_, {dispatch}) => {
     dispatch(logout());
   },
 );
@@ -54,19 +57,17 @@ export const authSlice = createSlice({
   initialState,
   reducers: {
     logout: (state: AuthState): AuthState => {
-      AsyncStorage.clear(); 
+      AsyncStorage.clear();
       return initialState;
     },
     setUser: (state: AuthState, action: PayloadAction<User>): AuthState => {
-      const tempState = { ...state };
+      const tempState = {...state};
 
       tempState.user = action.payload;
-      tempState.userKey = action.payload.sessionInfo?.username;
-
       return tempState;
     },
     setToken: (state: AuthState, action: PayloadAction<string>): AuthState => {
-      const tempState = { ...state };
+      const tempState = {...state};
 
       tempState.token = action.payload;
 
@@ -74,29 +75,33 @@ export const authSlice = createSlice({
     },
   },
   extraReducers(builder) {
-    builder.addCase(
-      loginAction.fulfilled,
-      (state: AuthState, action: PayloadAction<AuthState>) => {
-        if (action.payload.remember) {
-          AsyncStorage.setItem(
-            process.env.REACT_APP_ACCESS_TOKEN_KEY!,
-            action.payload.token,
-          );
-        }
+    builder
+      .addCase(
+        loginAction.fulfilled,
+        (state: AuthState, action: PayloadAction<AuthState>) => {
+          if (action.payload.remember) {
+            AsyncStorage.setItem(
+              process.env.REACT_APP_ACCESS_TOKEN_KEY!,
+              action.payload.token,
+            );
+          }
 
-        const tempState = { ...state };
+          const tempState = {...state};
 
-        tempState.token = action.payload.token;
-        tempState.user = action.payload.user;
-        tempState.userKey = action.payload.userKey;
-        tempState.remember = action.payload.remember;
+          tempState.token = action.payload.token;
+          tempState.user = action.payload.user;
+          tempState.userKey = action.payload.userKey;
+          tempState.remember = action.payload.remember;
 
-        return tempState;
-      },
-    );
+          return tempState;
+        },
+      )
+      .addCase(loginAction.rejected, (_, action) => {
+        console.log('error');
+      });
   },
 });
 
-export const { logout, setUser, setToken } = authSlice.actions;
+export const {logout, setUser, setToken} = authSlice.actions;
 
 export default authSlice.reducer;
