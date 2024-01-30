9ceba3dceed6efa169de74456fc986738823ceda
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-01-30 03:13:09 +0100

    search updated & fixed.

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,27 +1,36 @@
-import React, { useEffect } from 'react';
-import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
-import { gStyle } from '../constants';
+  import React, { useEffect, useState } from 'react';
+  import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+  import { gStyle } from '../constants';
 
-// components
-import Cast from '../components/Cast';
-import HeaderSearch from '../components/Header/HeaderSearch';
+  // components
+  import Cast from '../components/Cast';
+  import HeaderSearch from '../components/Header/HeaderSearch';
+import SearchScrollView from '../components/SearchScrollView';
+import data from '../mockData/data';
 
-function Search() {
-  const [search, setSearch] = React.useState('');
+  function Search() {
+    const [search, setSearch] = React.useState('');
+    const [searchedDataList, setSearchedDataList] = useState([] as { id: number; title: string; image: string; }[]);
+    const mockData = data.dumbData;
 
-  useEffect(()=>{
-    console.log(search)
-  },[search])
-  return (
-    <>
-      <TouchableWithoutFeedback accessible={false} onPress={Keyboard.dismiss}>
-        <View style={gStyle.container}>
-          <HeaderSearch setSearchText={setSearch} />
-        </View>
-      </TouchableWithoutFeedback>
-      <Cast />
-    </>
-  );
-}
+    useEffect(() => {
+      console.log(search);
+      const filteredData = mockData.filter(item =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setSearchedDataList(filteredData);
+      console.log(searchedDataList.length);
+    }, [search]);
+  
+    return (
+      <>
+          <View style={gStyle.container}>
+            <HeaderSearch setSearchText={setSearch} />
+            <SearchScrollView dataList={searchedDataList} />
+          </View>
+        <Cast />
+      </>
+    );
+  }
 
-export default Search;
+  export default Search;
