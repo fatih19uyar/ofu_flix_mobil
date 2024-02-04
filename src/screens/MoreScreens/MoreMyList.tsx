e2c72670cdf07cebd593de3a5ca6459aab6a0076
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2024-02-04 22:06:46 +0100

    MoreMyList added scrollview

--- src/screens/MoreScreens/MoreMyList.tsx
+++ src/screens/MoreScreens/MoreMyList.tsx
@@ -4,15 +4,21 @@ import { colors, gStyle } from '../../constants';
 
 // components
 import Header from '../../components/Header/Header';
+import ContentScrollView from '../../components/ContentScrollView';
+
+import { useAppSelector } from '../../common/hooks/useStore';
 
 const Container = styled.View`
   ${gStyle.container}
 `;
 
 const MoreMyList: React.FC = () => {
+  const myList = useAppSelector((state) => state.content.myList);
+
   return (
     <Container>
       <Header bg={colors.headerBarBg} showBack title="My List" />
+      <ContentScrollView dataList={myList} />
     </Container>
   );
 };
